63647b1fa1926da70c1f02391c8eddd3d0d35e49
Joymax Nascimento <joymaxnascimento@yahoo.com.br>
2021-03-25 23:15:11 -0300

    t

--- index.js
+++ index.js
@@ -3,10 +3,54 @@ const express = require('express');
 const server = express();
 server.use(express.json());
 
+server.use((req, res, next) => {
+  console.time('Request')
+})
+
+const  users = ['Joymax', 'Lívia', 'Peida']
+
+function checkUsersInArray(req, res, next){
+  const user = users[req.params.index]
+  if (!user){
+    return res.status(400).json({error: 'Index doesn\'t existis'})
+  }
+  req.user = user
+  return next()
+}
+
+function checkUsersExist(req, res, next){
+  if (!req.body.name){
+    return res.status(400).json({error: 'User name is invalid'})
+  }
+  return next()
+}
 
 server.get('/users/', (req, res) => {
-  return res.send('A rota esta up');
+  return res.json(users);
 });
 
+server.get('/users/:index', checkUsersInArray, (req, res) => {
+  return res.json(users[req.params.index]);
+});
+
+server.post('/users/', checkUsersExist, (req, res) => {
+  const { name } = req.body
+  users.push(name)
+  return res.status(201).json(users)
+})
+
+server.put('/users/:index', checkUsersInArray, checkUsersExist,(req, res) => {
+  const {index} = req.params
+  const {name} = req.body
+  users[index] = name
+
+  return res.json(users)
+})
+
+server.delete('/users/:index', checkUsersInArray,(req, res) => {
+  const {index} = req.params
+  users.splice(index, 1)
+  return res.json(users)
+})
 
 server.listen(3001);
